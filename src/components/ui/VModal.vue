5945361f77d2c12cb7142c40f34c347b27619f72
Anton Maryanskiy <Maryanskiy44@yandex.ru>
2023-05-01 20:48:10 +0300

    edit vmodal

--- src/components/ui/VModal.vue
+++ src/components/ui/VModal.vue
@@ -14,7 +14,7 @@ const closeModal = () => {
 <template>
   <div
     v-if="visible"
-    class="absolute h-screen w-full py-5 grid grid-rows-[auto,_1fr,_auto] gap-10 text-center bg-primary-dark"
+    class="absolute h-screen w-full py-5 grid grid-rows-[auto_1fr_auto] gap-10 text-center bg-primary-dark"
   >
     <p class="underline" @click="closeModal">Закрыть</p>
     <slot class="h-4/6" />
