f64102dde9e0dfae96562bc574d7f75f3242e23d
YOUR_NAME <YOUR_EMAIL>
2026-02-27 22:00:39 +0300

    ignore android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'how-well-do-you-know',
+  webDir: 'dist'
+};
+
+export default config;
